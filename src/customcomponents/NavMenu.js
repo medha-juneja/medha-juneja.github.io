61d2b1feaa1c750b6a48f3211c8100df0a953813
medha-juneja <medhaj001@gmail.com>
2020-02-09 01:37:08 +0100

    added comment to NavMenu component and changed height

--- src/customcomponents/NavMenu.js
+++ src/customcomponents/NavMenu.js
@@ -1,23 +1,15 @@
 /*eslint-disable*/
 import React from "react";
-import DeleteIcon from "@material-ui/icons/Delete";
-import IconButton from "@material-ui/core/IconButton";
-// react components for routing our app without refresh
-import { Link } from "react-router-dom";
-
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import Tooltip from "@material-ui/core/Tooltip";
 
 // @material-ui/icons
-import { Apps, CloudDownload, Email, Home, ListAlt } from "@material-ui/icons";
+import { Apps, Email, Home, ListAlt } from "@material-ui/icons";
 
 // core components
-import CustomDropdown from "components/CustomDropdown/CustomDropdown.js";
 import Button from "components/CustomButtons/Button.js";
-
 import styles from "assets/jss/material-kit-react/components/headerLinksStyle.js";
 
 const useStyles = makeStyles(styles);
@@ -25,113 +17,78 @@ const useStyles = makeStyles(styles);
 export function LeftHeaderLinks(props) {
   const classes = useStyles();
   return (
+    // Left links start
     <List className={classes.list}>
-
+      { /* Home Link start */}
       <ListItem className={classes.listItem}>
         <Button
           href="#"
           color="transparent"
+          size ="lg"
           target="_blank"
           className={classes.navLink}
         >
-          <Home className={classes.icons} /> Home
+          <Home className={classes.icons} />
+          Home
         </Button>
       </ListItem>
+      { /* Home Link end */}
 
+      { /* Resume Link start */}
       <ListItem className={classes.listItem}>
         <Button
           href="#"
           color="transparent"
+          size ="lg"
           target="_blank"
           className={classes.navLink}
         >
-          <ListAlt className={classes.icons} /> Resume
+          <ListAlt className={classes.icons} />
+          Resume
         </Button>
       </ListItem>
+      { /* Resume Link end */}
 
     </List>
     );
+    // Left Links end
   }
 
 export function RightHeaderLinks(props) {
   const classes = useStyles();
   return (
+    // Right links start
     <List className={classes.list}>
-
+      { /* Blog Link start */}
       <ListItem className={classes.listItem}>
         <Button
           href="#"
           color="transparent"
+          size ="lg"
           target="_blank"
           className={classes.navLink}
         >
-          <Apps className={classes.icons} /> Blog
+          <Apps className={classes.icons} />
+          Blog
         </Button>
       </ListItem>
+      { /* Blog Link end */}
 
+      { /* Contact Link start */}
       <ListItem className={classes.listItem}>
         <Button
           href="#"
           color="transparent"
+          size ="lg"
           target="_blank"
           className={classes.navLink}
         >
-          <Email className={classes.icons} /> Contact Me
+          <Email className={classes.icons} />
+          Contact Me
         </Button>
       </ListItem>
-
+      { /* Contact Link end */}
       </List>
     );
+    // Right links end
   }
-
-
-
-      // <ListItem className={classes.listItem}>
-      //   <Button
-      //     href="https://www.creative-tim.com/product/material-kit-react?ref=mkr-navbar"
-      //     color="transparent"
-      //     target="_blank"
-      //     className={classes.navLink}
-      //   >
-      //     <CloudDownload className={classes.icons} /> Download
-      //   </Button>
-      // </ListItem>
-      // <ListItem className={classes.listItem}>
-      //   {/*<Tooltip title="Delete">
-      //     <IconButton aria-label="Delete">
-      //       <DeleteIcon />
-      //     </IconButton>
-      //   </Tooltip>*/}
-      //   <Tooltip
-      //     id="instagram-twitter"
-      //     title="Follow us on twitter"
-      //     placement={window.innerWidth > 959 ? "top" : "left"}
-      //     classes={{ tooltip: classes.tooltip }}
-      //   >
-      //     <Button
-      //       href="https://twitter.com/CreativeTim?ref=creativetim"
-      //       target="_blank"
-      //       color="transparent"
-      //       className={classes.navLink}
-      //     >
-      //       <i className={classes.socialIcons + " fab fa-twitter"} />
-      //     </Button>
-      //   </Tooltip>
-      // </ListItem>
-      // <ListItem className={classes.listItem}>
-      //   <Tooltip
-      //     id="instagram-facebook"
-      //     title="Follow us on facebook"
-      //     placement={window.innerWidth > 959 ? "top" : "left"}
-      //     classes={{ tooltip: classes.tooltip }}
-      //   >
-      //     <Button
-      //       color="transparent"
-      //       href="https://www.facebook.com/CreativeTim?ref=creativetim"
-      //       target="_blank"
-      //       className={classes.navLink}
-      //     >
-      //       <i className={classes.socialIcons + " fab fa-facebook"} />
-      //     </Button>
-      //   </Tooltip>
-      // </ListItem>
--- src/customcomponents/Patents.js
+++ src/customcomponents/Patents.js
@@ -22,39 +22,39 @@ const Patents = () => {
                       <h2 className={classes.cvheading}>Patents and Publications</h2>
                     </div>
                       { /* Patents and Pubs subheading container start */}
-                      <div style={{textAlign:"left", marginLeft:"70px"}}>
-                        { /* Patents heading */}
-                        <h3 className={classes.cvshorttitle}>Patents</h3>
-                        { /* Description start */}
-                        <p className={classes.description}>
-                            { /* Patents description start */}
-                            <ul style={{lineHeight: 1.7}}>
-                              <li><b>An immersive telepresence system for the radiological workflow</b>, DE, Submitted in April 2019.</li>
-                                <li><b>Technique for performing a quality assessment for a medical image</b>, DE, Submitted in January 2019.</li>
-                                <li><b>Technique for configuring a medical imaging device,</b>, DE, Submitted in January 2019.</li>
-                            </ul>
-                            { /* Patents description end */}
-                        <br/>
+                    <div style={{textAlign:"left", marginLeft:"70px"}}>
+                      { /* Patents heading */}
+                      <h3 className={classes.cvshorttitle}>Patents</h3>
+                      { /* Description start */}
+                      <p className={classes.description}>
+                          { /* Patents description start */}
+                          <ul style={{lineHeight: 1.7}}>
+                            <li><b>An immersive telepresence system for the radiological workflow</b>, DE, Submitted in April 2019.</li>
+                              <li><b>Technique for performing a quality assessment for a medical image</b>, DE, Submitted in January 2019.</li>
+                              <li><b>Technique for configuring a medical imaging device,</b>, DE, Submitted in January 2019.</li>
+                          </ul>
+                          { /* Patents description end */}
+                      <br/>
 
                         { /* Publications heading */}
-                          <h3 className={classes.cvshorttitle}>Publications</h3>
-                            { /* Publications description start */}
-                            <ul style={{lineHeight: 1.7}}>
-                              <li>M. Juneja, M. Bode-Hofmann, K. S. Haong, S. Meißner, V. Merkel, J. Vogt, N.Wilke, A. Wolff, and T. Hartkens,
-                                “Image quality assessments,” in Bildverarbeitungfür die Medizin 2019,
-                                H. Handels, T. M. Deserno, A. Maier, K. H. Maier-Hein,C. Palm, and T. Tolxdorff, Eds., Wiesbaden:
-                                Springer Fachmedien Wiesbaden, 2019,pp. 225–230,isbn: 978-3-658-25326-4.
-                                </li>
-                              <li>M. Juneja and P. Grover, “Improved segmentation of occluded and adjoining vehiclesin traffic surveillance videos,”
-                                in Sixth International Conference on Machine Vision(ICMV 2013),
-                                International Society for Optics and Photonics, vol. 9067, 2013,p. 90670M.
+                        <h3 className={classes.cvshorttitle}>Publications</h3>
+                          { /* Publications description start */}
+                          <ul style={{lineHeight: 1.7}}>
+                            <li>M. Juneja, M. Bode-Hofmann, K. S. Haong, S. Meißner, V. Merkel, J. Vogt, N.Wilke, A. Wolff, and T. Hartkens,
+                              “Image quality assessments,” in Bildverarbeitungfür die Medizin 2019,
+                              H. Handels, T. M. Deserno, A. Maier, K. H. Maier-Hein,C. Palm, and T. Tolxdorff, Eds., Wiesbaden:
+                              Springer Fachmedien Wiesbaden, 2019,pp. 225–230,isbn: 978-3-658-25326-4.
                               </li>
-                            </ul>
-                        { /* Publications description end */}
+                            <li>M. Juneja and P. Grover, “Improved segmentation of occluded and adjoining vehiclesin traffic surveillance videos,”
+                              in Sixth International Conference on Machine Vision(ICMV 2013),
+                              International Society for Optics and Photonics, vol. 9067, 2013,p. 90670M.
+                            </li>
+                          </ul>
+                      { /* Publications description end */}
                       </p>
-                      { /* Description end */}
+                    { /* Description end */}
                     </div>
-                    { /* Patents and Pubs subheading container end */}
+                  { /* Patents and Pubs subheading container end */}
                 </div>
                 { /* Patents and Pubs end */}
               </GridItem>
