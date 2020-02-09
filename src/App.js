c0dbe2393a77e1e096bfe7dbd2c565c878a2c1a2
medha-juneja <medhaj001@gmail.com>
2020-02-09 01:41:01 +0100

    cleaned App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Header from './components/Header/Header.js';
-import  {RightHeaderLinks, LeftHeaderLinks} from './customcomponents/NavMenu.js';
-import ProfilePic from './customcomponents/ProfilePic.js';
-import Resume from './customcomponents/Resume.js';
-import Education from './customcomponents/Education.js';
+import Header from 'components/Header/Header.js';
+import  {RightHeaderLinks, LeftHeaderLinks} from 'customcomponents/NavMenu.js';
+import ProfilePic from 'customcomponents/ProfilePic.js';
+import Resume from 'customcomponents/Resume.js';
+import Education from 'customcomponents/Education.js';
 import SkillSet from 'customcomponents/SkillSet.js';
 import Patents from 'customcomponents/Patents.js';
-import './App.css';
+import 'App.css';
 
 class App extends React.Component {
   render() {
