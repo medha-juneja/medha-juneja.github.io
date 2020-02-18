e09fdc8561bfcb9981677919bef3e789b148bbde
medha-juneja <medhaj001@gmail.com>
2020-02-18 22:53:25 +0100

    Updates

---
+++ precache-manifest.a342dddf36749a129344df4462a87883.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "266768f98c3e5d59f2118520517a672d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01c9dc0d6a0975749a44",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f15a88a48f12cc7aa866",
+    "url": "/static/js/2.89687e02.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "/static/js/2.89687e02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01c9dc0d6a0975749a44",
+    "url": "/static/js/main.270d9617.chunk.js"
+  },
+  {
+    "revision": "4225718bbd6a4d5e4d7b",
+    "url": "/static/js/runtime-main.3318ade5.js"
+  },
+  {
+    "revision": "457f03c083e612810f08d5b6a3783078",
+    "url": "/static/media/cvpicgrey.457f03c0.jpg"
+  }
+]);
