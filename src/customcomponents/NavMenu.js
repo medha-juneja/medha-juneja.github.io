6bbf5d84cf6a281559208e1a90f99a875514162e
medha-juneja <medhaj001@gmail.com>
2020-03-04 12:49:58 +0100

    changed patent icon

--- src/customcomponents/NavMenu.js
+++ src/customcomponents/NavMenu.js
@@ -6,7 +6,7 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 
 // @material-ui/icons
-import { Apps, Email, Home, ListAlt, Settings, Stars } from "@material-ui/icons";
+import { Apps, Email, Home, ListAlt, Settings, Assignment } from "@material-ui/icons";
 import {Link} from "react-scroll";
 // core components
 import Button from "components/CustomButtons/Button.js";
@@ -90,7 +90,7 @@ export function RightHeaderLinks(props) {
           target="_blank"
           className={classes.navLink}
         >
-          <Stars className={classes.icons} />
+          <Assignment className={classes.icons} />
           Patents
         </Button>
       </ListItem>
